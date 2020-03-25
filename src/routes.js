5966353d661560674ab9696a83c9b317c648018a
Luis Enrique <luyo.lemp@gmail.com>
2020-03-24 22:55:06 -0300

    create route for list incident of specific ONG

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ const express = require('express')
 
 const OngController = require('./controllers/OngController')
 const IncidentController = require('./controllers/IncidentController')
+const ProfileController = require('./controllers/ProfileController')
 
 const routes = express.Router()
 
@@ -15,5 +16,6 @@ routes.get('/incidents', IncidentController.index)
 routes.post('/incidents', IncidentController.create)
 routes.delete('/incidents/:id', IncidentController.delete)
 
+routes.get('/profile', ProfileController.index)
 //exportando a variavel do arquivo
 module.exports = routes;
