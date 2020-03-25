31458296b52887ee102c9f5fc61040e759d8cbfd
Luis Enrique <luyo.lemp@gmail.com>
2020-03-24 22:45:44 -0300

    create route for delete incident

--- src/controllers/IncidentController.js
+++ src/controllers/IncidentController.js
@@ -23,5 +23,28 @@ module.exports = {
     })
 
     return res.json({ id })
+  },
+
+  async delete(req, res) {
+    const { id } = req.params;
+    const ong_id = req.headers.authorization
+
+    console.log({ong_id})
+
+    const incident = await connection('incidents')
+    .where('id', id)
+    .select('ong_id')
+    .first();
+    
+    console.log({incident})
+
+    if(incident.ong_id !== ong_id) {
+      return res.status(401).json({ error: 'Operação não permitida' })
+    }
+
+    await connection('incidents').where('id', id).delete()
+    
+    //204 deu sucesso mas não tem conteudo para enviar
+    return res.status(204).send()
   }
 }
--- src/routes.js
+++ src/routes.js
@@ -13,6 +13,7 @@ routes.post('/ongs', OngController.create)
 
 routes.get('/incidents', IncidentController.index)
 routes.post('/incidents', IncidentController.create)
+routes.delete('/incidents/:id', IncidentController.delete)
 
 //exportando a variavel do arquivo
 module.exports = routes;
