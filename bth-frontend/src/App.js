042888647d94395d242402064d5df12ec6bef1fa
Luis Enrique <luyo.lemp@gmail.com>
2020-03-25 09:53:33 -0300

    [FRONTEND] added assets folder and styles to pages

--- bth-frontend/src/App.js
+++ bth-frontend/src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
+import Logon from './pages/Logon'
+
 function App() {
   return (
-    <h1>Hello World</h1>
+    <Logon />
   );
 }
 
---
+++ bth-frontend/src/pages/Logon/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import './styles.css'
+
+export default function Logon () {
+  return (
+    <h1> Hello World Logon </h1>
+  )
+}
