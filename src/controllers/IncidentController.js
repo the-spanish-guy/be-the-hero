992b10f5580ba5841f5de4ed0ee4a96da0dafca4
Luis Enrique <luyo.lemp@gmail.com>
2020-03-24 23:21:56 -0300

    create a simple pagination and showing total rows on header

--- src/controllers/IncidentController.js
+++ src/controllers/IncidentController.js
@@ -3,7 +3,20 @@ const connection = require('../database/connection')
 module.exports = {
 
   async index(req, res) {
-    const incidents = await connection('incidents').select('*')
+
+    const { page = 1 } = req.query;
+    
+    //buscanod o total de incidentes do banco de dados
+    const [count] = await connection('incidents')
+    .count()
+
+    const incidents = await connection('incidents')
+    .limit(5)
+    .offset((page - 1) * 5)
+    .select('*')
+
+    //setando no header o total de incidentes
+    res.header('X-Total-Count', count['count(*)'])
 
     return res.json( incidents )
   },
