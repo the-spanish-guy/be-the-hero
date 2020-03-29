83a04675028518fb4df790b461eb371d5fb460b3
Luis Enrique <luyo.lemp@gmail.com>
2020-03-28 21:43:56 -0300

    [BACKEND] added validation for routes

--- bth-backend/src/index.js
+++ bth-backend/src/index.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const cors = require('cors')
 const routes = require('./routes')
+const { errors } = require('celebrate')
 
 const app = express() // iniciando o app
 
 app.use(cors())
 app.use(express.json()) //avisando ao express para que converta o tudo em json
 app.use(routes)
-
+app.use(errors())
 
 app.listen(3333) //ouvindo a porta 3333
